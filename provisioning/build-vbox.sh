6e8fdeebd30f11e420877e60ecb82afe582493c0
Dav Clark <davclark@berkeley.edu>
2014-08-08 12:58:47 -0700

    Moving on to further development
    build-vbox.sh now works on OS X / old Bash
    Adding proper dependencies for networkx on py2.7

--- provisioning/build-vbox.sh
+++ provisioning/build-vbox.sh
@@ -1,1 +1,7 @@
-packer build -only=virtualbox-iso BCE-14.04-amd64.json |& tee packer-out.log
+# Note, 2>&1 redirects to wherever stdout is directed when it gets processed
+# Pipes get parsed before redirects
+# This is why we place it before the pipe
+# See: http://mywiki.wooledge.org/BashFAQ/055
+# ps - Bash is just a crazy sorthand for some C code
+
+packer build -only=virtualbox-iso BCE-14.04-amd64.json  2>&1 | tee packer-out.log
