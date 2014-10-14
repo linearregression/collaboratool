041643a460a85cc8d7a7d9ef25c9aedbe6804522
Carl Boettiger <cboettig@gmail.com>
2014-10-14 12:20:12 -0700

    update Dockerfile comments

---
+++ provisioning/userconf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Set defaults for environmental variables in case they are undefined
+USER=${USER:=rstudio}
+PASSWORD=${PASSWORD:=rstudio}
+EMAIL=${EMAIL:=rstudio@example.com}
+## Configure user account name and password (used by rstudio)
+useradd -m $USER && echo "$USER:$PASSWORD" | chpasswd
+## Give user access over their own home directory
+chown -R $USER:$USER /home/$USER
+## Let user write to /usr/local/lib/R/site.library
+addgroup $USER staff
+
